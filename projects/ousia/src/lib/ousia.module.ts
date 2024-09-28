71c4d003ef1c19964ebd9613be821dee30ea527e
Paul Gordts <paul.gordts@live.com>
2024-09-28 16:14:01 +0200

    Ousia library generated

---
+++ projects/ousia/src/lib/ousia.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { OusiaComponent } from './ousia.component';
+
+
+
+@NgModule({
+  declarations: [
+    OusiaComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    OusiaComponent
+  ]
+})
+export class OusiaModule { }
---
+++ projects/ousia/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ousia
+ */
+
+export * from './lib/ousia.service';
+export * from './lib/ousia.component';
+export * from './lib/ousia.module';
