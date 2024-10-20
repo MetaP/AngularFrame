b84caa13d4ab68dfef204d844ae510bc9ee61574
Paul Gordts <paul.gordts@live.com>
2024-10-20 16:50:11 +0200

    isNonBlankString utility function added

--- projects/ousia/src/lib/utilities/utility.ts
+++ projects/ousia/src/lib/utilities/utility.ts
@@ -17,6 +17,15 @@ export function isUndefined(value: any): boolean {
 }
 
 /**
+ * Determines whether the specified value is a string with at least one character that is not a whitespace or line terminator.
+ * @param value A value to test.
+ * @returns true if the specified value is a a string with at least one character that is not a whitespace or line terminator; otherwise false.
+ */
+export function isNonBlankString(value: any) : boolean {
+	return (typeof value === 'string' && value.trim().length > 0);
+}
+
+/**
  * Returns true if the specified value is not a string or a string that contains only whitespace and line terminators characters;
  * otherwise false.
  * @param value A value to test.
--- projects/ousia/src/lib/utilities/utitlity.spec.ts
+++ projects/ousia/src/lib/utilities/utitlity.spec.ts
@@ -1,4 +1,4 @@
-import { isBlankOrNotString, isDefined, isUndefined } from "./utility";
+import { isDefined, isUndefined, isNonBlankString, isBlankOrNotString } from "./utility";
 
 describe('utility.ts', () => {
 
@@ -29,6 +29,30 @@ describe('utility.ts', () => {
 		});
 		it('should return true if value is an empty string', () => {
 			expect(isUndefined('')).toBeFalse();
+		});
+	});
+
+	describe('isNonBlankString(value)', () => {
+		it('should return false if value is null', () => {
+			expect(isNonBlankString(null)).toBeFalse();
+		});
+		it('should return false if value is undefined', () => {
+			expect(isNonBlankString(undefined)).toBeFalse();
+		});
+		it('should return false if value is an empty string', () => {
+			expect(isNonBlankString('')).toBeFalse();
+		});
+		it('should return false if value is a string containing only spaces', () => {
+			expect(isNonBlankString('     ')).toBeFalse();
+		});
+		it('should return false if value is a string containing only spaces, tabs and newlines', () => {
+			expect(isNonBlankString(' \t \n  ')).toBeFalse();
+		});
+		it('should return false if value is number', () => {
+			expect(isNonBlankString(1)).toBeFalse();
+		});
+		it('should return true if value is a non-blank string', () => {
+			expect(isNonBlankString('text')).toBeTrue();
 		});
 	});
 
